14e2b4dfc7f1e7a6f0963e576b0a57daf35d7190
Arne Dumarey <dumarey.arne@gmail.com>
2017-02-20 19:17:49 +0100

    Added tests and set ncrunch config for v3

---
+++ src/UniqueFileRecordsComparer/UniqueFileRecordsComparer.Core.Tests/When_constructing_ColumnValues.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace UniqueFileRecordsComparer.Core.Tests
+{
+    public class When_constructing_ColumnValues
+    {
+        [Fact]
+        public void Then_expected_values_are_set()
+        {
+            var expectedheader = "expectedHeader";
+            var expectedValues = new List<string> {"abc", "foo"};
+
+            var columnValues = new ColumnValues(expectedheader, expectedValues);
+
+            columnValues.Header.Should().Be(expectedheader);\
+            columnValues.Values.Should().BeEquivalentTo(expectedValues);
+        }
+    }
+}
